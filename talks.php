9b3fe89d61e927979b76909600f2b26322f1e147
m-osama <mohammedosama@gmail.com>
2019-03-07 16:24:03 +0200

    responsive talk media card

--- talks.php
+++ talks.php
@@ -22,19 +22,21 @@ include './header.php';
         <div class="container">
 
             <div class="talk1">
-                
+                <!-- talk #001 start -->
                 <h3> #001: How to Think Like a Front-End Developer</h3>
                 <!-- <p>video by: wordpress</p> -->
                 <div class="row">
-
-                    <div class="col s12 m6 offset-m3"><!-- card start -->
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/tI0MGJe_ojU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </div><!-- card end -->
+                    <!-- card start -->
+                    <div class="col s12 m6 offset-m3">
+                        <div class="video-container">
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/tI0MGJe_ojU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div><!-- card end -->
+                    </div>
                 </div>
             </div>
-            
         </div>
         <div class="space"></div>
+        <!-- talk #001 end -->
     </section>
 
     <?php
